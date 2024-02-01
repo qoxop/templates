5ab6e10a17b5ad6b8c34c5831b1b7fc6ee9b0638
张集伟 <zhangjw01@mingyuanyun.com>
2024-02-01 09:03:31 +0800

    添加 chrome 插件模板项目

---
+++ packages/chrome-extension/README.md
@@ -0,0 +1,27 @@
+# __PACKAGE_NAME__
+
+使用 bun 开发 chrome 插件
+
+## 文档
+- [Chrome 插件开发文档](https://developer.chrome.com/?hl=zh-cn)
+- [bun.js 文档](https://bun.sh/)
+
+## 安装
+```bash
+bun install
+```
+
+## 命令
+
+开发
+
+```bash
+bun start
+# bun index.ts --watch
+```
+
+构建
+```bash
+bun build
+# bun index.ts
+```
---
+++ packages/chrome-extension/src/content/index.tsx
@@ -0,0 +1,11 @@
+if (process.env.NODE_ENV === 'development') {
+  let socket = new WebSocket("ws://127.0.0.1:8080");
+  socket.onmessage = async function (event) {
+    if (event.data.toString() === 'reload') {
+      setTimeout(() => {
+        window.location.reload()
+      }, 200);
+    }
+  };
+}
+console.log('content script');
