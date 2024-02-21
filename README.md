c2de6d758546c4230c606fa221e066df76f7e91b
张集伟 <zhangjw01@mingyuanyun.com>
2024-02-21 13:40:40 +0800

    创建脚本

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # templates
+
 项目模板
+
+## 项目创建命令
+
+使用 `curl`
+```bash
+curl -sSL [https://raw.githubusercontent.com/qoxop/templates/main/create.sh] | bash -s -- -p=项目模板名称 -n=项目名称 -t=标签名称-可选
+```
+
+使用 `wget`
+```bash
+wget -O - [https://raw.githubusercontent.com/qoxop/templates/main/create.sh] | bash -s -- -p=项目模板名称 -n=项目名称 -t=标签名称-可选
+```
+
+## 模板列表
+
+### Chrome 插件项目
+
+```bash
+curl -sSL [https://raw.githubusercontent.com/qoxop/templates/main/create.sh] | bash -s -- -p=chrome-extension -n=my-chrome-extension
+# wget -O - [https://raw.githubusercontent.com/qoxop/templates/main/create.sh] | bash -s -- -p=chrome-extension -n=my-chrome-extension
+```
---
+++ create.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# 参数解析
+for arg in "$@"
+do
+    case $arg in
+        -p=*|--project=*)
+        PROJECT="${arg#*=}"
+        shift # Remove --project from processing
+        ;;
+        -n=*|--name=*)
+        NAME="${arg#*=}"
+        shift # Remove --name from processing
+        ;;
+        -t=*|--tag=*)
+        TAG="${arg#*=}"
+        shift # Remove --tag from processing
+        ;;
+        *)
+        OTHER_ARGUMENTS+=("$1")
+        shift # Remove generic argument from processing
+        ;;
+    esac
+done
+
+# 如果没有提供-name参数，则将其设置为与 project 相同的值
+if [ -z "$NAME" ]; then
+    NAME=$PROJECT
+fi
+
+# 检查是否提供了必要的参数
+if [ -z "$PROJECT" ]; then
+    echo "Error: Project name is required."
+    exit 1
+fi
+
+# 创建临时目录并克隆git仓库
+TEMP_DIR=$(mktemp -d)
+
+# 根据是否指定了tag来执行不同的git clone操作
+if [ -z "$TAG" ]; then
+    # 如果没有指定tag，克隆主分支的最新提交
+    git clone --depth 1 git@github.com:qoxop/templates.git "$TEMP_DIR"
+else
+    # 如果指定了tag，克隆特定的tag
+    git clone --branch "$TAG" --depth 1 git@github.com:qoxop/templates.git "$TEMP_DIR"
+fi
+
+# 拷贝仓库中的packages目录到当前目录中的指定名称目录
+if [ -d "$TEMP_DIR/packages/$PROJECT" ]; then
+    cp -r "$TEMP_DIR/packages/$PROJECT" "./$NAME"
+else
+    echo "Error: Project '$PROJECT' does not exist in the repository."
+    exit 1
+fi
+
+# 删除临时目录
+rm -rf "$TEMP_DIR"
+
+echo "Done. '$PROJECT' has been copied to './$NAME'."
