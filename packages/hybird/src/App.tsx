7e1b4a75ffc1424732d23871b563ff1e65f65b65
张集伟 <zhangjw01@mingyuanyun.com>
2024-03-04 14:06:25 +0800

    add tpl code

--- packages/hybird/src/App.tsx
+++ packages/hybird/src/App.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button"
+import Mail from "@/container/mail"
+import { accounts, mails } from "@/components/data"
 
 function App() {
+
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="hidden flex-col md:flex">
+      <Mail
+        accounts={accounts}
+        mails={mails}
+        defaultLayout={undefined}
+        defaultCollapsed={undefined}
+        navCollapsedSize={4}
+      />
     </div>
   )
 }
